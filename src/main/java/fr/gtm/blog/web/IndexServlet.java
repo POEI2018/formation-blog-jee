e7df910d3535abb75f8be2dac89f9d4f0873512f
POEI2018 <renc7595@gmail.com>
2018-06-27 11:08:17 +0200

    modif du dao

--- src/main/java/fr/gtm/blog/web/IndexServlet.java
+++ src/main/java/fr/gtm/blog/web/IndexServlet.java
@@ -3,13 +3,13 @@ package fr.gtm.blog.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.gtm.blog.business.ArticleService;
+import fr.gtm.blog.dao.ArticleRepository;
 
 /**
  * Point d'entrée dans l'application par l'URL '/articles'. Permet de traiter
@@ -20,6 +20,8 @@ public class IndexServlet extends AutoWiredServlet {
 
 	@Autowired 
 	private ArticleService service ; 
+	@Autowired
+	private ArticleRepository repo ; 
 
 	@Override
 	public void init() throws ServletException {
@@ -39,7 +41,7 @@ public class IndexServlet extends AutoWiredServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		request.getSession().setAttribute("listArticle",
 				// Accès à l'instance unique de ArticleService.
-				service.getArticles());
+				repo.findAll());
 		        this.getServletContext()
 				.getRequestDispatcher("/WEB-INF/views/index.jsp")
 				.forward(request, response);
