5d1e526fbac2153ef4f1df5823b0268cfffbd697
POEI2018 <renc7595@gmail.com>
2018-07-02 16:56:36 +0200

    methodes delete et update dans webservice

--- src/main/java/fr/gtm/blog/web/ArticleWebService.java
+++ src/main/java/fr/gtm/blog/web/ArticleWebService.java
@@ -3,11 +3,15 @@ package fr.gtm.blog.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.gtm.blog.business.ArticleService;
@@ -33,5 +37,16 @@ public class ArticleWebService {
 	@GetMapping("/{id}")
 	public Article readOne(@PathVariable Integer id) {
        return this.articleService.read(id) ;
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteArticle(@PathVariable Integer id) {
+		this.articleService.delete(id); 
+	}
+	
+	@PutMapping("/{id}")
+	public Article update(@RequestBody Article article) {
+		return this.articleService.edit(article);
 	}
 }
